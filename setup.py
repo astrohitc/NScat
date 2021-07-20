7b06d4a1299875c0df584a61ba7b4a49570b0a0b
Rohit Chinchwadkar <63839559+astrohitc@users.noreply.github.com>
2021-07-20 21:57:42 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,33 +1,11 @@
-import pathlib
-from setuptools import setup, find_packages
+from setuptools import setup
 
-HERE = pathlib.Path(__file__).parent
-
-VERSION = '0.1.0'
-PACKAGE_NAME = 'your_package'
-AUTHOR = 'You'
-AUTHOR_EMAIL = 'you@email.com'
-URL = 'https://github.com/you/your_package'
-
-LICENSE = ''
-DESCRIPTION = 'Describe your package in one sentence'
-LONG_DESCRIPTION = (HERE / "README.md").read_text()
-LONG_DESC_TYPE = "text/markdown"
-
-INSTALL_REQUIRES = [
-      'numpy',
-      'pandas'
-]
-
-setup(name=PACKAGE_NAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      long_description_content_type=LONG_DESC_TYPE,
-      author=AUTHOR,
-      license=LICENSE,
-      author_email=AUTHOR_EMAIL,
-      url=URL,
-      install_requires=INSTALL_REQUIRES,
-      packages=find_packages()
-      )
+setup(name='Test1',
+version='1.0',
+description='Testing installation of Package',
+url='#',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+packages=['Test1'],
+zip_safe=False)
